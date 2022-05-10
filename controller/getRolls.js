9605b7913701b4b1dc02df68c59c92ba832b646f
Victor Machado <victor.machado.machado@gmail.com>
2022-05-10 21:19:51 +0200

    deleteRolls and getRolls

---
+++ controller/getRolls.js
@@ -0,0 +1,29 @@
+const {Players, Rolls} = require('../models/dices');
+
+module.exports = async function (req, res) {
+    try {
+        // Check if user exists
+        const user = await Players.findOne({
+            where: { username: req.params.id },
+            });
+        if(!user) return res.status(400).send({ status: "fail", message: "user not found"}); // 400 - Bad request
+
+        let rolls = await Rolls.findAll({
+            where: { playerId: user.id },
+          });
+
+        res.status(200).send({
+            status: "success",
+            data: {
+                username: user.username,
+                rolls
+            }
+        });
+
+    } catch (err) {
+        res.status(500).send({
+            status: 'error',
+            message: err.message
+        })
+    }
+};
--- routes/players.js
+++ routes/players.js
@@ -5,13 +5,15 @@ const getPlayers = require('../controller/getPlayers');
 const addPlayer = require('../controller/addPlayer');
 const updatePlayer = require('../controller/updatePlayer');
 const rollDices = require('../controller/rollDices');
+const deleteRolls = require('../controller/deleteRolls');
+const getRolls = require('../controller/getRolls');
 
 router.post('/', addPlayer);
 router.put('/', updatePlayer);
 router.post('/:id/games', rollDices);
-router.delete('/:id/games', (req, res) => res.send('DELETE /players/:id/games'));
+router.delete('/:id/games', deleteRolls);
 router.get('/', getPlayers);
-router.get('/:id/games', (req, res) => res.send('GET /players/:id/games'));
+router.get('/:id/games', getRolls);
 router.get('/ranking', (req, res) => res.send('GET /players/ranking'));
 router.get('/ranking/loser', (req, res) => res.send('GET /players/ranking/loser'));
 router.get('/ranking/winner', (req, res) => res.send('GET /players/ranking/winner'));
