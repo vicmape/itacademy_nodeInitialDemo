f93f5b3d8141bb0fdd681e981d68c69ac72c8500
Victor Machado <victor.machado.perez@gmail.com>
2022-05-24 11:54:16 +0200

    dark mode by default

--- frontend/scripts/matrix.js
+++ frontend/scripts/matrix.js
@@ -5,13 +5,13 @@ function matrix() {
     if (current === "Wake up") {
         document.getElementById("matrix").innerHTML = 'Keep dreaming';
 
-        r.style.setProperty("--color", "lime");
-        r.style.setProperty("--background-color", "black");
-
-    } else {
-        document.getElementById("matrix").innerHTML = 'Wake up';
-
+        
         r.style.setProperty("--color", "black");
         r.style.setProperty("--background-color", "white");
+    } else {
+        document.getElementById("matrix").innerHTML = 'Wake up';
+        
+        r.style.setProperty("--color", "lime");
+        r.style.setProperty("--background-color", "black");
     }
 }
